a0a3ecaac37afaa2c2a3444aea713e4e2f5d27e2
muanimate <muanimate@outlook.com>
2024-10-15 15:20:30 +0800

    新增电话格式函数

--- src/utils/string/string.ts
+++ src/utils/string/string.ts
@@ -28,7 +28,19 @@ const replaceParams = (str: string, params: { [key: string]: string }): string =
 	return str.replace(/{(\w+)}/g, (_, key) => params[key] || `{${key}}`);
 }
 
+
+/**
+ * Formats a phone number string by inserting spaces between groups of digits.
+ *
+ * @param phone - The phone number string to format. It should be a string of digits.
+ * @returns The formatted phone number string with spaces inserted.
+ */
+const formatPhone = (phone: string): string => {
+	return phone.replace(/(\d{3})(\d{4})(\d{4})/, '$1 $2 $3');
+}
+
 export {
 	thousandseparator,
-	replaceParams
+	replaceParams,
+	formatPhone
 }
