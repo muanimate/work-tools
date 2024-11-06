01d08acb649c8d6aa732532007f149c380a713fb
muanimate <muanimate@outlook.com>
2024-11-06 19:54:48 +0800

    新增type

--- src/types/addkey.d.ts
+++ src/types/addkey.d.ts
@@ -1,3 +1,12 @@
 export type ListItem = {
 	[key: string]: any;
 }
+
+export type TreeNode<T, K, U> = {
+	[key: string]: any;
+	children?: TreeNode<T, K, U>[];
+	// eslint-disable-next-line no-undef
+	[key in K]: string;
+	// eslint-disable-next-line no-undef
+	[key in U]: string;
+}
